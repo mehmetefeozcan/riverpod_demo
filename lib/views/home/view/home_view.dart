1e1369fc37b97242d8de3fa182e1ae1b38ab3304
mehmetefeozcan <ozcanmehmetefe@gmail.com>
2023-06-23 10:32:22 +0300

    done lang

--- lib/views/home/view/home_view.dart
+++ lib/views/home/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_demo/core/utils/language/language.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:riverpod_demo/views/home/viewModel/home_view_model.dart';
 
 // ignore: must_be_immutable
 class HomeView extends ConsumerWidget {
@@ -11,23 +12,26 @@ class HomeView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeNotifer = ref.watch(appThemeProvider);
+    final homeController = ref.watch(homeProvider);
     final language = ref.watch(languageProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
+        title: Text(
+            "${AppLocalizations.of(context)!.title} ${homeController.title}"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("${AppLocalizations.of(context)!.currentTheme}:"),
+            Text(AppLocalizations.of(context)!.changeTheme),
             ElevatedButton(
               onPressed: () {
                 themeNotifer.changeTheme();
               },
-              child: Text(AppLocalizations.of(context)!.helloWorld.toString()),
+              child: Text(AppLocalizations.of(context)!.helloWorld),
             ),
+            Text(AppLocalizations.of(context)!.changeLang),
             ElevatedButton(
               onPressed: () {
                 if (language == const Locale('tr')) {
@@ -35,8 +39,9 @@ class HomeView extends ConsumerWidget {
                 } else {
                   ref.read(languageProvider.notifier).tr();
                 }
+                homeController.changeTitle();
               },
-              child: Text(AppLocalizations.of(context)!.lang.toString()),
+              child: Text(AppLocalizations.of(context)!.lang),
             ),
           ],
         ),
